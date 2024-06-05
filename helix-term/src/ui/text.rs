5963cccf8f9c7d70ed361966e062495186c28221
Gustav Svensk <grulfen3@gmail.com>
2024-06-05 18:45:56 +0200

    Allow scroll in Text Component

--- helix-term/src/ui/text.rs
+++ helix-term/src/ui/text.rs
@@ -5,48 +5,36 @@ use helix_view::graphics::Rect;
 
 pub struct Text {
     pub(crate) contents: tui::text::Text<'static>,
-    size: (u16, u16),
-    viewport: (u16, u16),
 }
 
 impl Text {
     pub fn new(contents: String) -> Self {
         Self {
             contents: tui::text::Text::from(contents),
-            size: (0, 0),
-            viewport: (0, 0),
         }
     }
 }
 
 impl From<tui::text::Text<'static>> for Text {
     fn from(contents: tui::text::Text<'static>) -> Self {
-        Self {
-            contents,
-            size: (0, 0),
-            viewport: (0, 0),
-        }
+        Self { contents }
     }
 }
 
 impl Component for Text {
-    fn render(&mut self, area: Rect, surface: &mut Surface, _cx: &mut Context) {
+    fn render(&mut self, area: Rect, surface: &mut Surface, cx: &mut Context) {
         use tui::widgets::{Paragraph, Widget, Wrap};
 
-        let par = Paragraph::new(&self.contents).wrap(Wrap { trim: false });
-        // .scroll(x, y) offsets
+        let mut par = Paragraph::new(&self.contents).wrap(Wrap { trim: false });
+        if let Some(scroll) = cx.scroll {
+            par = par.scroll((scroll as u16, 0))
+        }
 
         par.render(area, surface);
     }
 
-    fn required_size(&mut self, viewport: (u16, u16)) -> Option<(u16, u16)> {
-        if viewport != self.viewport {
-            let width = std::cmp::min(self.contents.width() as u16, viewport.0);
-            let height = std::cmp::min(self.contents.height() as u16, viewport.1);
-            self.size = (width, height);
-            self.viewport = viewport;
-        }
-        Some(self.size)
+    fn required_size(&mut self, _viewport: (u16, u16)) -> Option<(u16, u16)> {
+        Some((self.contents.width() as u16, self.contents.height() as u16))
     }
 }
 
